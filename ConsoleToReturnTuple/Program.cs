63db39139775b28f96b4b55de480fb895c69d697
Carter Lankford <calankford@outlook.com>
2018-06-14 12:46:55 -0500

    Add project files.

---
+++ ConsoleToReturnTuple/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleToReturnTuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var tupTestTuple = Tuple.Create("hello", 1, true);
+            Console.WriteLine("string: {0}", tupTestTuple.Item1);
+            Console.WriteLine("int: {0}", tupTestTuple.Item2.ToString());
+            Console.WriteLine("boolean: {0}", tupTestTuple.Item3.ToString());
+            Console.ReadLine();
+        }
+    }
+}
