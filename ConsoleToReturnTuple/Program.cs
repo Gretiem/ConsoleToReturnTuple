9a91c879db3f106cef9cce03a7a1324b7de1775a
Carter Lankford <calankford@outlook.com>
2018-06-14 14:12:39 -0500

    third change, should conflict

--- ConsoleToReturnTuple/Program.cs
+++ ConsoleToReturnTuple/Program.cs
@@ -16,7 +16,7 @@ namespace ConsoleToReturnTuple
             Console.WriteLine("int: {0}", tupTestTuple.Item2.ToString());
             Console.WriteLine("boolean: {0}", tupTestTuple.Item3.ToString());
             Console.ReadLine();
-            //A change
+            //The Third change, should result in a conflict
 
             //Another change
         }
